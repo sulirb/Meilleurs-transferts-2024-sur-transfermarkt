a67416fc895b8ef918dba009da3b8cd14c318eaf
sulirb <121161440+sulirb@users.noreply.github.com>
2024-07-20 18:40:18 +0200

    webdriver implementation

--- api/index.py
+++ api/index.py
@@ -1,91 +1,139 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template_string
+import concurrent.futures
 
 app = Flask(__name__)
-gecko_driver_path = "C:/Users/sirba/OneDrive/Documents/webdrivers/geckodriver.exe"
+gecko_driver_path = "../webdriver/geckodriver.exe"
+
 options = webdriver.FirefoxOptions()
 options.add_argument('--headless')
-
 options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
 
-@app.route('/', methods=['GET'])
-
-def run_script():
+def fetch_transfer_data(url):
     driver = None
     try:
         driver = webdriver.Firefox(service=Service(gecko_driver_path), options=options)
-
-        base_url = "https://www.transfermarkt.fr/transfers/saisontransfers/statistik?land_id=0&ausrichtung=&spielerposition_id=&altersklasse=&leihe=&transferfenster=&saison-id=0&plus=1&page="
-        num_pages = 6
-
-        all_transfers = []
-
-        for page in range(1, num_pages + 1):
-            url = f"{base_url}{page}"
-            driver.get(url)
+        driver.get(url)
     
-            html_content = driver.page_source
-            soup = BeautifulSoup(html_content, "html.parser")
+        html_content = driver.page_source
+        soup = BeautifulSoup(html_content, "html.parser")
 
-            transfer_tab = soup.find("table", {"class": "items"})
-            if not transfer_tab:
-                continue
+        transfer_tab = soup.find("table", {"class": "items"})
+        if not transfer_tab:
+            return []
 
-            player_profiles = transfer_tab.find_all("table", {"class": "inline-table"})
-    
-            position_list = [
+        player_profiles = transfer_tab.find_all("table", {"class": "inline-table"})
+
+        position_list = [
                 "Gardien de but", "Libéro", "Défenseur central", "Arrière gauche", "Arrière droit", 
                 "Milieu défensif", "Milieu central", "Milieu droit", "Milieu gauche", "Milieu offensif", 
                 "Ailier gauche", "Ailier droit", "Deuxième attaquant", "Avant-centre"
             ]
 
-            positions = []
-            for player_profile in player_profiles:
-                found_position = None
-                for position in position_list:
-                    position_td = player_profile.find_all("tr")[1].find("td", string=position)
-                    if position_td:
-                        found_position = position
-                        break
-                if found_position:
-                    positions.append(found_position)
+        positions = []
+        players = []
+        clubs = []
+        montants = []
 
-            players = []
-            for player in transfer_tab.find_all("img", {"class": "bilderrahmen-fixed"}):
-                players.append(player["alt"])
+        for player_profile in player_profiles:
+            found_position = None
+            for position in position_list:
+                position_td = player_profile.find_all("tr")[1].find("td", string=position)
+                if position_td:
+                    found_position = position
+                    break
+            if found_position:
+                positions.append(found_position)
 
-            clubs = []
-            for club in transfer_tab.find_all("img", {"class": "tiny_wappen"}):
-                clubs.append(club["alt"])
-
-            montants = []
-            for montant in transfer_tab.select("td.rechts.hauptlink, td.rechts.hauptlink.bg-gruen_20"):
-                montants.append(montant.text)
-
-            def chunked(iterable, n):
-                """Yield successive n-sized chunks from iterable."""
-                for i in range(0, len(iterable), n):
-                    yield iterable[i:i + n]
-        
-            complete_transfer = []
-            for player, montant, club_pair, position in zip(players, montants, chunked(clubs, 2), positions):
-                club_1 = club_pair[0]
-                club_2 = club_pair[1] if len(club_pair) > 1 else 'N/A'  # Handle case where clubs list is odd
-                complete_transfer.append(f'<div class="inline"><div class="player">{player}</div> <div class="position">({position})</div> <div class="transfer">{club_1} ------> {club_2} (prix: {montant})</div></div>\n')
+        for player in transfer_tab.find_all("img", {"class": "bilderrahmen-fixed"}):
+            players.append(player["alt"])
             
-            all_transfers.extend(complete_transfer)
+        for club in transfer_tab.find_all("img", {"class": "tiny_wappen"}):
+            clubs.append(club["alt"])
+            
+        for montant in transfer_tab.select("td.rechts.hauptlink, td.rechts.hauptlink.bg-gruen_20"):
+            montants.append(montant.text)
 
-        response_html = "\n".join(all_transfers)
+        def chunked(iterable, n):
+            """Yield successive n-sized chunks from iterable."""
+            for i in range(0, len(iterable), n):
+                yield iterable[i:i + n]
+        
+        complete_transfer = []
+        for player, montant, club_pair, position in zip(players, montants, chunked(clubs, 2), positions):
+            club_1 = club_pair[0]
+            club_2 = club_pair[1] if len(club_pair) > 1 else 'N/A'  # Handle case where clubs list is odd
+            complete_transfer.append(f'<div class="transfer"><div class="player">{player}</div> <div class="position">({position})</div> <div class="transfer-details">{club_1} ------> {club_2} (fee: {montant})</div></div>\n')
+            
+        return complete_transfer
+
+    finally:
+        if driver:
+            driver.quit()
+
+def run_script(base_url, num_pages = 6):
+    urls=[f"{base_url}{page}" for page in range(1, num_pages + 1)]
+    #base_url = "https://www.transfermarkt.fr/transfers/saisontransfers/statistik?land_id=0&ausrichtung=&spielerposition_id=&altersklasse=&leihe=&transferfenster=&saison-id=0&plus=1&page="
+    #base_url_per_position = base_url.replace("spielerposition_id=", "spielerposition_id=14")
+    #urls=[f"{base_url}{page}" for page in range(1, num_pages + 1)]
+    #urls = [f"{base_url_per_position}{page}" for page in range(1, num_pages + 1)]
+    
+    all_transfers = []
+    try:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = executor.map(fetch_transfer_data, urls)
+            for result in results:
+                all_transfers.extend(result)
+    
+        template = """
+        <!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Meilleurs transferts</title>
+            <link rel="stylesheet" href="{{ url_for('static', filename='css/style.css') }}">
+        </head>
+        <body>
+            <div class="container">
+                {{ content|safe }}
+            </div>
+        </body>
+        </html>
+        """
+
+        response_html = render_template_string(template, content="\n".join(all_transfers)) 
         return response_html
 
     except Exception as e:
         return jsonify(error=str(e))
 
-    finally:
-        if driver:
-            driver.quit()
+@app.route('/', methods=['GET'])
+def transferts():
+    base_url = "https://www.transfermarkt.fr/transfers/saisontransfers/statistik?land_id=0&ausrichtung=&spielerposition_id=&altersklasse=&leihe=&transferfenster=&saison-id=0&plus=1&page="
+    return run_script(base_url)
+
+@app.route('/goalkeepers', methods=['GET'])
+def transfertsGardiens():
+    base_url = "https://www.transfermarkt.fr/transfers/saisontransfers/statistik?land_id=0&ausrichtung=&spielerposition_id=1&altersklasse=&leihe=&transferfenster=&saison-id=0&plus=1&page="
+    return run_script(base_url)
+
+@app.route('/defenders', methods=['GET'])
+def transfertsDefenseurs():
+    base_url = "https://www.transfermarkt.fr/transfers/saisontransfers/statistik?land_id=0&ausrichtung=Abwehr&spielerposition_id=&altersklasse=&leihe=&transferfenster=&saison-id=0&plus=1&page="
+    return run_script(base_url)
+
+@app.route('/midfields', methods=['GET'])
+def transfertsMilieux():
+    base_url = "https://www.transfermarkt.fr/transfers/saisontransfers/statistik?land_id=0&ausrichtung=Mittelfeld&spielerposition_id=&altersklasse=&leihe=&transferfenster=&saison-id=0&plus=1&page="
+    return run_script(base_url)
+
+@app.route('/forwards', methods=['GET'])
+def transfertsAttaquants():
+    base_url = "https://www.transfermarkt.fr/transfers/saisontransfers/statistik?land_id=0&ausrichtung=Sturm&spielerposition_id=&altersklasse=&leihe=&transferfenster=&saison-id=0&plus=1&page="
+    return run_script(base_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
